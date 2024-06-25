011e4f8c93c7821ecffec6bf931e8230629e2fa8
mxriuki <mwendav37@gmail.com>
2024-06-25 11:21:13 +0300

    complete

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function receivesAFunction(callback) {
-    callback()
+const returnFirstTwoDrivers = array => {
+    return array.slice(0, 2)
 }
-function returnsANamedFunction(){
-    return function named() {
-        console.log(Lapilly)
+
+const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(i) {
+    return function(fare) {
+      return fare * i;
     }
 }
 
-function returnsAnAnonymousFunction() {
-    return function() {
-        console.log(Nameless)
-    }
-}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(driversArray, driversFunction) {
+    return driversFunction(driversArray);
+  };
